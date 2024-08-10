e8486c6cbaf0836efbe2edf82d8427f284a6f085
Vinodpuri Goswami <vinodpurigoswami735@gmail.com>
2024-08-10 20:56:08 +0530

    completed hometask-03

---
+++ tests/emulation.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect, devices } from '@playwright/test';
+
+test.use({
+  ...devices['iPhone 13'],
+});
+
+test('iphone test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await page.locator('[data-test="checkout"]').click();
+  await page.locator('[data-test="continue"]').click();
+  await page.locator('[data-test="firstName"]').fill('vinodpuri Goswami');
+  await page.locator('[data-test="lastName"]').fill('vinodpurigoswami@gmail.com');
+  await page.locator('[data-test="postalCode"]').fill('400080');
+  await page.locator('[data-test="continue"]').click();
+  await page.locator('[data-test="finish"]').click();
+  await expect(page.locator('[data-test="complete-header"]')).toContainText('Thank you for your order!');
+});
